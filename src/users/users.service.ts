a2df53a198700f25a3cd149fde66af832680d6d6
farkuy <darkuy@mail.ru>
2024-05-15 00:32:53 +0300

    добавил авторизиацию и регистрацию

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,18 +2,33 @@ import { Injectable } from '@nestjs/common';
 import {InjectModel} from "@nestjs/sequelize";
 import {User} from "./user.model";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {RolesService} from "../roles/roles.service";
 
 @Injectable()
 export class UsersService {
-    constructor (@InjectModel(User) private userRepository: typeof User){
+    constructor (@InjectModel(User) private userRepository: typeof User,
+                 private rolesService: RolesService){
     }
     async createUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto);
+        const role = await this.rolesService.getRoleByValue("USER");
+        await user.$set('roles', [role.id])
+        user.roles = [role]
         return user;
     }
 
     async getAllUser() {
-        const users = await this.userRepository.findAll();
+        const users = await this.userRepository.findAll( {
+            include: {all: true}
+        } );
         return users;
+    }
+
+    async getUserByEmail(email: string) {
+        const user = await this.userRepository.findOne({
+            where: {email},
+            include: {all: true}
+        })
+        return user;
     }
 }
